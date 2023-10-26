d8a3223584670ccffc66fada8d831f198ab6ed34
user <user@usernoMacBook-Pro.local>
2023-10-26 10:02:04 +0900

    render-build.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rails db:seed_fu
+bundle exec rake db:seed_fu
 
 # デフォルトの設定
 # bundle install; bundle exec rake assets:precompile; bundle exec rake assets:clean; bundle exec rake db:migrate; bundle exec rails db:seed
