c9ec6b3bf54fdc8fb8f0e9dd66084b40536aae5f
user <user@usernoMacBook-Pro.local>
2023-11-01 10:07:19 +0900

    seedデータの再挿入

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,9 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake db:migrate
 # seedデータの再挿入
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-# bundle exec rake db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:seed
 # デフォルトの設定
 # bundle install; bundle exec rake assets:precompile; bundle exec rake assets:clean; bundle exec rake db:migrate; bundle exec rails db:seed
