7b33af1a36578c2b9c97f1a15748ebbba790c0a9
user <user@usernoMacBook-Pro.local>
2023-11-07 12:22:02 +0900

    render-build.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
 # seedデータの再挿入
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 # bundle exec rake db:seed
